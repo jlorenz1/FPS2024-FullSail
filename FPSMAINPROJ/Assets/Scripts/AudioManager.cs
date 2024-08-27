714387f63f28e7c54047deb18cb11b387056b54c
Joel Lorenz <117110394+jlorenz1@users.noreply.github.com>
2024-08-27 18:15:28 -0400

    Audio manager setup and ready to use
    I made a audiomanager singleton for us to use. There is a method already to play a audio clip. This is setup for 2d sounds, 3d will need to be setup still once added for enemies.

---
+++ FPSMAINPROJ/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _audioInstance;
+    [SerializeField] public AudioSource _audioSource;
+    public static AudioManager audioInstance
+    {
+        get
+        {
+            if (_audioInstance == null)
+            {
+                Debug.LogError("AudioManager is null");
+            }
+            return _audioInstance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_audioInstance != null && _audioInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _audioInstance = this;
+        }
+
+
+    }
+
+    void playAudio(AudioClip audClip, float vol)
+    {
+        _audioSource.PlayOneShot(audClip, vol);
+    }
+}
