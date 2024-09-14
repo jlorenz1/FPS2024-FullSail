8e809f6ef75e2167310001832561f424b336be09
Emily Underwood <egunderwood@student.fullsail.edu>
2024-09-14 13:51:20 -0400

    Memorization Puzzle is fully functional

--- FPSMAINPROJ/Assets/Scripts/Room Components and Puzzles/MemoryPuzzleController.cs
+++ FPSMAINPROJ/Assets/Scripts/Room Components and Puzzles/MemoryPuzzleController.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class MemoryPuzzleController : MonoBehaviour
 {
     public GameObject door;
+    public GameObject placingDisplaysBit; //used for placing display tiles
+
+    //display tiles
+    public GameObject display1;
+    public GameObject display2;
+    public GameObject display3;
+    public GameObject display4;
+    public GameObject display5;
+    public GameObject display6;
+    public GameObject display7;
+    public GameObject display10;
+    public GameObject display11;
+    public GameObject display14;
+    public GameObject display15;
+    public GameObject display17;
+    public GameObject display18;
+    public GameObject display20;
 
     //pattern options for sequence player needs to complete
     int[] pattern1 = { 3, 6, 11, 15, 20 };
@@ -57,8 +74,18 @@ public class MemoryPuzzleController : MonoBehaviour
             pattern = pattern4;
         }
 
-        //!delete later, testing purposes only
-        pattern = pattern1;
+        //displaying the sequence tiles
+        //placingDisplaysBit is in there to make it easier to traverse the array
+        GameObject[] displayTiles = {display1,  display2, display3, display4, display5, display6, display7, 
+        placingDisplaysBit, placingDisplaysBit, display10, display11, placingDisplaysBit, placingDisplaysBit, 
+        display14, display15, placingDisplaysBit, display17, display18, placingDisplaysBit, display20};
+
+        Vector3 location = placingDisplaysBit.transform.position;
+        for (int i = 0; i < pattern.Length; i++) //moving chosen tiles so the player can see them
+        {
+            displayTiles[pattern[i]-1].transform.position = location;
+            location += new Vector3(1, 0, 0);
+        }
     }
 
     //updates sequence array and handles when player steps on incorrect tile
