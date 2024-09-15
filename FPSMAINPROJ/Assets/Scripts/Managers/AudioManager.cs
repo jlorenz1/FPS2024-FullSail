5433ae6a3e2e445c2a091b0b07c55b16a7f9f77c
jlorenz1 <joellorenz888@gmail.com>
2024-09-15 16:25:20 -0400

    Game music in.
    I have one music clip being played across the game for now, I plan on adding a new clip to play when going into main scene.

--- FPSMAINPROJ/Assets/Scripts/Managers/AudioManager.cs
+++ FPSMAINPROJ/Assets/Scripts/Managers/AudioManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager _audioInstance;
 
     public AudioSource _SFXAudio;
+
+    public AudioSource _musicAudio;
+
+    public AudioClip _mainMenuBGMusic;
+    public float _mainMenuBGVol;
+    public AudioClip _gameBGMusic;
+    public float _gameBGVol;
+    
   
     public static AudioManager audioInstance
     {
@@ -14,7 +23,15 @@ public class AudioManager : MonoBehaviour
         {
             if (_audioInstance == null)
             {
-                Debug.LogError("AudioManager is null");
+                _audioInstance = FindObjectOfType<AudioManager>();
+                if(_audioInstance == null)
+                {
+                    Debug.Log("AudioManager is null");
+                    GameObject audioManagerObject = new GameObject("AudioManager");
+                    _audioInstance = audioManagerObject.AddComponent<AudioManager>();
+                    DontDestroyOnLoad(audioManagerObject);
+                }
+              
             }
             return _audioInstance;
         }
@@ -29,14 +46,37 @@ public class AudioManager : MonoBehaviour
         else
         {
             _audioInstance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
-        
     }
 
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            if (_musicAudio != _gameBGMusic && _gameBGMusic != null)
+            {
+                _musicAudio.volume = _gameBGVol;
+            }
+        }
+    }
 
+    private void Start()
+    {
+        if(_musicAudio != null && _mainMenuBGMusic != null)
+        {
+            _musicAudio.clip = _mainMenuBGMusic;
+            _musicAudio.volume = _mainMenuBGVol;
+            _musicAudio.Play();
+        }
+    }
     public void playSFXAudio(AudioClip audClip, float vol)
     {
-        _SFXAudio.PlayOneShot(audClip, vol);
+        if(_SFXAudio !=  null && audClip != null)
+        {
+            _SFXAudio.PlayOneShot(audClip, vol);
+        }
+     
     }
 }
